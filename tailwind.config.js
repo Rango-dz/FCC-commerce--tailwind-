97b77108f60acfcd30afe0b42ff948bec97ab854
david <4635854+Rango-dz@users.noreply.github.com>
2020-05-04 02:59:40 +0100

    v 1.0

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  purge: [],
+  theme: {
+    extend: {
+      theme: {
+        colors: {
+        gobgh: '#f8f8f8',
+        gobh: '#c6c6c6',
+        goblack: '#222222',
+        gobgimgh1: '#f8f8f8',
+        gobgimgh2: '#f1f1f1',
+        },
+
+        boxShadow: {
+          goshadow: '0 1px 1px rgba(0,0,0,0.1)',
+          goshadowbig: '0 1px 3px 0 rgba(32,33,36,0.28)',
+        }
+      }
+    },
+  },
+  variants: {},
+  plugins: [],
+}
